55fedaa4b465f1226163cdaf1d46cf6388066e0f
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-10-24 23:11:05 -0400

    Updated miner to not bug if no container

--- role.miner.js
+++ role.miner.js
@@ -1,22 +1,18 @@
 module.exports = {
-    // a function to run the logic for this role
     run: function (creep) {
-        // get source
         let source = Game.getObjectById(creep.memory.sourceId);
-        // find container next to source
         let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
             filter: s => s.structureType == STRUCTURE_CONTAINER
         })[0];
 
-        // if creep is on top of the container
-        if (creep.pos.isEqualTo(container.pos)) {
-            // harvest source
-            creep.harvest(source);
+        if (container != undefined) {
+            if (creep.pos.isEqualTo(container.pos)) {
+                creep.harvest(source);
+            }
+            else {
+                creep.moveTo(container);
+            }
         }
-        // if creep is not on top of the container
-        else {
-            // move towards it
-            creep.moveTo(container);
-        }
+        else {creep.getEnergy(false, true);}
     }
 };

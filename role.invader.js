e6b8a4d33ff3a9d2d8c6c482270971a81109051e
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-10-25 15:26:14 -0400

    Adjusted invader to prioritise structures

--- role.invader.js
+++ role.invader.js
@@ -1,31 +1,20 @@
 module.exports = {
-	// a function to run the logic for this role
 	/** @param {Creep} creep */
 	run: function (creep) {
-		// if in target room
 		if (creep.room.name == creep.memory.target) {
-			// find closes hostile creep
-			var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-			// if one is found...
+			let target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
 			if (target != undefined) {
-				// try to stab it
 				if (creep.attack(target) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(target);
 				}
 			} else {
-				target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-				// try to stab it
+				target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 				if (creep.attack(target) == ERR_NOT_IN_RANGE) {
-					// But, if it's not close enough to stab, try to shoot it
 					creep.moveTo(target);
 				}
 			}
-		}
-		// if not in target room
-		else {
-			// find exit to target room
+		} else {
 			var exit = creep.room.findExitTo(creep.memory.target);
-			// move to exit
 			creep.moveTo(creep.pos.findClosestByRange(exit));
 		}
 	},

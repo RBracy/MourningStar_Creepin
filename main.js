7989fd5d78458bded85351ad5712204acdd2b2b0
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-10-26 19:52:56 -0400

    Jonestown is over

--- main.js
+++ main.js
@@ -47,12 +47,6 @@ for (let spawnName in Game.spawns) {
 		Game.spawns[spawnName].memory.minInvaders = {};
 	}
 }
-
-for (let name in Game.creeps) {
-	if (Game.creeps[name].memory.role == 'remoteMiner') {
-		Game.creeps[name].suicide;
-	}
-}
 module.exports.loop = function () {
 	const tickTock = Memory.tickTock;
 	if (tickTock < 5) {

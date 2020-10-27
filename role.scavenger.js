21395b97c0b5aa8403fdeea30cf5396c18359c86
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-10-27 12:07:26 -0400

    Updates to logistics network

--- role.scavenger.js
+++ role.scavenger.js
@@ -1,4 +1,4 @@
-var roleSchlepper = require('role.schlepper');
+const roleLorry = require('role.lorry');
 
 module.exports = {
 	// a function to run the logic for this role
@@ -6,20 +6,20 @@ module.exports = {
 	run: function (creep) {
 		let target = creep.room.storage;
 		if (
-			creep.memory.working == false &&
+			creep.memory.working == true &&
 			creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0
 		) {
-			creep.memory.working = true;
+			creep.memory.working = false;
 		} else if (
-			creep.memory.working == true &&
+			creep.memory.working == false &&
 			creep.store.getFreeCapacity() == 0
 		) {
-			creep.memory.working = false;
+			creep.memory.working = true;
 		}
 
-		if (creep.memory.working == true) {
-			if (creep.getSalvage() == -1 && creep.room.memory.linksEnabled == true) {
-				roleSchlepper.run(creep);
+		if (creep.memory.working == false) {
+			if (creep.getSalvage() == -1) {
+				roleLorry.run(creep);
 			}
 		} else {
 			if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {

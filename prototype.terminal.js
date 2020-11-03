9e72e9cf8fb116ac62af843c58c661c94800202b
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-11-03 00:35:10 -0500

    still again

--- prototype.terminal.js
+++ prototype.terminal.js
@@ -22,7 +22,7 @@ StructureTerminal.prototype.marketSell = function () {
 	} else {
 		const saleItem = this.memory.sellOrder.itemType;
 		const minPrice = this.memory.sellOrder.minPrice;
-		const quantity = this.memory.sellOrder.quantity;
+		var quantity = this.memory.sellOrder.quantity;
 
 		if (this.memory.sellOrder.quantity == 0) {
 			return;
@@ -52,7 +52,7 @@ StructureTerminal.prototype.marketSell = function () {
 				let result = Game.market.deal(orders[0].id, 200, this.room.name);
 				if (result == 0) {
 					console.log('Order completed successfully');
-					this.memory.sellorder.quantity - 200;
+					quantity -= 200;
 				}
 			}
 		}

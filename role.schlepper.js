69af31902ac5e82041b288f99b12369cb64ac7ca
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-11-04 15:35:51 -0500

    Schlepper appears to finally work

--- role.schlepper.js
+++ role.schlepper.js
@@ -1,37 +1,92 @@
+const requisitions = [
+	'H',
+	'O',
+	'U',
+	'L',
+	'K',
+	'Z',
+	'X',
+	'G',
+	'silicon',
+	'metal',
+	'biomass',
+	'mist',
+	'energy',
+	'power',
+];
+
 module.exports = {
 	/** @param {Creep} creep */
 	run: function (creep) {
-		let link = creep.room.find(FIND_STRUCTURES, {
+		const link = creep.room.find(FIND_STRUCTURES, {
 			filter: (link) =>
 				link.structureType == STRUCTURE_LINK && link.memory.target == true,
 		});
 
-		if (
-			creep.memory.working == true &&
-			creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0
-		) {
+		const storage = creep.room.storage;
+		const terminal = creep.room.terminal;
+
+		if (creep.memory.working == true && creep.store.getUsedCapacity() == 0) {
 			creep.memory.working = false;
+			delete creep.memory.resourceType; // Delete memory of the resource just unloaded
 		} else if (
 			creep.memory.working == false &&
 			creep.store.getFreeCapacity() == 0
 		) {
 			creep.memory.working = true;
-		} else if (
-			creep.memory.working == false &&
-			link[0].store.getUsedCapacity(RESOURCE_ENERGY) == 0
-		) {
-			creep.memory.working = true;
 		}
 
-		if (creep.memory.working == true) {
-			let structure = creep.room.storage;
-			if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(structure);
+		if (creep.memory.working == false) {
+			if (link[0].store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+				switch (creep.withdraw(link[0], RESOURCE_ENERGY)) {
+					case OK:
+						creep.memory.working = true;
+						break;
+					case ERR_NOT_IN_RANGE:
+						creep.moveTo(link[0]);
+						break;
+				}
+			} else {
+				for (let resType of requisitions) {
+					if (
+						terminal.memory.requisitions[resType] > 0 &&
+						storage.store.getUsedCapacity(resType) > 0
+					) {
+						switch (creep.withdraw(storage, resType)) {
+							case OK:
+								creep.memory.resourceType = resType;
+								break;
+							case ERR_NOT_IN_RANGE:
+								creep.moveTo(storage);
+								break;
+						}
+					}
+				}
 			}
 		} else {
-			if (link[0].store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
-				if (creep.withdraw(link[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(link[0]);
+			var resource = creep.memory.resourceType;
+			if (
+				terminal.memory.requisitions['energy'] <= 0 &&
+				creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0
+			) {
+				switch (creep.transfer(storage, RESOURCE_ENERGY)) {
+					case OK:
+						break;
+					case ERR_NOT_IN_RANGE:
+						creep.moveTo(storage);
+						break;
+				}
+			} else if (
+				resource != undefined &&
+				creep.store.getUsedCapacity(resource) > 0
+			) {
+				switch (creep.transfer(terminal, resource)) {
+					case OK:
+						terminal.memory.requisitions[resource] -= creep.store.getCapacity();
+						break;
+					case ERR_NOT_IN_RANGE:
+						creep.moveTo(terminal);
+						break;
 				}
 			}
 		}

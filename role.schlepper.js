4bde172f9f573a4042ed25c276959a4546138c8b
Ryan F Bracy <65883274+RBracy@users.noreply.github.com>
2020-11-05 14:18:42 -0500

    That looks better

--- role.schlepper.js
+++ role.schlepper.js
@@ -66,9 +66,9 @@ module.exports = {
 		} else {
 			var resource = creep.memory.resourceType;
 			if (
-				terminal.memory.requisitions['energy'] <= 0 &&
-				creep.memory.resourceType == undefined &&
-				creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0
+				creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+				(terminal.memory.requisitions['energy'] <= 0 ||
+					creep.memory.resourceType == undefined)
 			) {
 				switch (creep.transfer(storage, RESOURCE_ENERGY)) {
 					case OK:
